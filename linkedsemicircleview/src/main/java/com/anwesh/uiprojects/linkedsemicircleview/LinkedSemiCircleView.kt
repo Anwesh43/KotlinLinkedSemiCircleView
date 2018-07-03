5614d65bae675ce902419dfbca53e3b841b7df41
Anwesh43 <anweshthecool0@gmail.com>
2018-07-03 10:27:56 +0530

    adding LinkedSemiCircleView class

--- linkedsemicircleview/src/main/java/com/anwesh/uiprojects/linkedsemicircleview/LinkedSemiCircleView.kt
+++ linkedsemicircleview/src/main/java/com/anwesh/uiprojects/linkedsemicircleview/LinkedSemiCircleView.kt
@@ -13,3 +13,21 @@ import android.graphics.Color
 
 val SEMI_CIRCLE_NODES = 5
 val color : Int = Color.parseColor("#512DA8")
+
+class LinkedSemiCircleView (ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
