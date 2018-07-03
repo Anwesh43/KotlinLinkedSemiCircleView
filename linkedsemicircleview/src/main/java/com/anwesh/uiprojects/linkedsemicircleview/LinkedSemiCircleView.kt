98655c56f5ca1f13232f0ae2f9e593f554f41ae1
Anwesh43 <anweshthecool0@gmail.com>
2018-07-03 10:23:58 +0530

    adding dependencies, number of nodes and color

--- linkedsemicircleview/src/main/java/com/anwesh/uiprojects/linkedsemicircleview/LinkedSemiCircleView.kt
+++ linkedsemicircleview/src/main/java/com/anwesh/uiprojects/linkedsemicircleview/LinkedSemiCircleView.kt
@@ -3,3 +3,13 @@ package com.anwesh.uiprojects.linkedsemicircleview
 /**
  * Created by anweshmishra on 03/07/18.
  */
+
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Color
+
+val SEMI_CIRCLE_NODES = 5
+val color : Int = Color.parseColor("#512DA8")
