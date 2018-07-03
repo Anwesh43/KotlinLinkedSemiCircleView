96be7510639e5071cf16c1876e14aacda1d8877b
Anwesh43 <anweshthecool0@gmail.com>
2018-07-03 11:30:44 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkedsemicircleview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkedsemicircleview/MainActivity.kt
@@ -2,10 +2,12 @@ package com.anwesh.uiprojects.kotlinlinkedsemicircleview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.linkedsemicircleview.LinkedSemiCircleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        LinkedSemiCircleView.create(this)
     }
 }
